41c9b5eee7fa01ef5503f2ebb8b6ffa2bef7b0b0
Przemek <p.ostrowinski@gmail.com>
2017-06-30 20:26:18 +0200

    fix rwd in navigation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     function scrollTopAnimation(goTop) {
-         $('html, body').animate({
+        $('html, body').animate({
             scrollTop: goTop
         }, 800);
     }
@@ -9,9 +9,27 @@ $(document).ready(function () {
     //navigation scroll down
     $('.nav-links').click(function (event) {
         event.preventDefault();
-        const getLink = $(this).attr('href'),
+        var getLink = $(this).attr('href'),
             goTop = $(getLink).offset().top;
-            scrollTopAnimation(goTop);
+        
+        scrollTopAnimation(goTop);
+    });
+    
+    //slide down jumbotron header when navigation is active
+    $('.navbar-toggle').click(function () {
+        var $hasCollapsed = $('.navbar-toggle').hasClass('collapsed'),
+            $jumboContainer = $('.jumbotron').find('.container'),
+            $windowWidth = $(window).width();
+        
+        if ($hasCollapsed && ($windowWidth < 768)) {
+            $jumboContainer.css('min-height', 'calc(100vh + 150px)');
+            $('.motto').css('transform', 'translateY(50%)');
+            $('.scroll-down-button').css('bottom', '0');
+        } else {
+            $jumboContainer.css('min-height', 'calc(100vh)');
+            $('.scroll-down-button').css('bottom', '8rem');
+            $('.motto').css('transform', 'translateY(-50%)');
+        }
     });
     
     //hide arrow after load website
@@ -19,13 +37,23 @@ $(document).ready(function () {
     
     //scroll website top from bottom-right arrow
     $('.scroll-top').click(function () {
-        const goTop = $('html').offset().top;
+        var goTop = $('html, body').offset().top;
         scrollTopAnimation(goTop);
     });
 
     //visibility bottom-right arrow
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 350) $('.scroll-top').fadeIn();
-        else $('.scroll-top').fadeOut();
+        if ($(this).scrollTop() > 350) {
+            $('.scroll-top').fadeIn();
+        } else {
+            $('.scroll-top').fadeOut();
+        }
+    });
+    
+    $('.scroll-down').click(function (e) {
+        e.preventDefault();
+        var scrollNextSection = $('.jumbotron')['0'].scrollHeight;
+        
+        scrollTopAnimation(scrollNextSection);
     });
 });
